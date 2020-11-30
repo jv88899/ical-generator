57041960597222ef313c167057946f79db58d7cb
jv88899 <jv88899@gmail.com>
2020-11-30 09:13:59 -0600

    added functionality to generate iCal

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import "./style.css";
 
 export default function App() {
   const [iCalText, setiCalText] = useState("");
-  const [url, setURL] = useState('')
-  const [password, setPassword] = useState('')
+  const [url, setURL] = useState("");
+  const [password, setPassword] = useState("");
 
-  const generateiCalText = () => {}
+  const generateiCalText = () => {
+    const newiCal = `Thank you for registering for this class. \\n \\nYou will receive an email with important information and instructions prior to the start of the class. \\n \\nWebex Link: ${url} \\nPassword: ${password}`;
+    setiCalText(newiCal)
+  };
+
+  const handleChange = e => {
+    if (e.target.name === "url") setURL(e.target.value);
+    if (e.target.name === "password") setPassword(e.target.value);
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
+    generateiCalText()
   };
 
   return (
@@ -17,11 +26,16 @@ export default function App() {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="url">URL</label>
-          <input type="text" name="url" />
+          <input type="text" name="url" onChange={handleChange} value={url} />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="text" name="password" />
+          <input
+            type="text"
+            name="password"
+            onChange={handleChange}
+            value={password}
+          />
         </div>
         <button type="submit">Create iCal Text</button>
       </form>
